926640258aee181f303ba7004e7185abae46c374
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-08-24 18:16:41 -0300

    shiftweb - Springboot

---
+++ shiftweb/src/main/java/br/com/fiap/shiftweb/controller/HelloApiController.java
@@ -0,0 +1,20 @@
+package br.com.fiap.shiftweb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloApiController {
+	
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello-api";
+	}
+	
+	@GetMapping("/hello2")
+	public String hello2() {
+		return "hello 2 api";
+	}
+
+}
